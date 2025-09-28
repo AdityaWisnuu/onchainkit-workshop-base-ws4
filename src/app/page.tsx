417a7e3c3e992f833877271e79e2ca186477fb4a
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-09-28 13:08:37 +0700

    feat: add miniapp dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import HomePage from "@/components/pages/home";
+import { sdk } from "@farcaster/miniapp-sdk";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
+
   return (
     <>
       <HomePage />
